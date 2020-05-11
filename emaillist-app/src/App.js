6c981ac934de6cc0bb77e4400789d078b487fcfe
hur-gilhaeng <hgh8401@gmail.com>
2020-05-11 20:36:33 +0900

    lifecircle & API 데이터 통신

--- emaillist-app/src/App.js
+++ emaillist-app/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import EmaillistApp from './EmaillistApp';
-import data from './data.json';
 
 export default class App extends React.Component {
     render() {
         return (
-            <EmaillistApp emails={ data }/>
+            <EmaillistApp/>
         );
     }
-} 
+}
--- emaillist-app/src/EmaillistApp.js
+++ emaillist-app/src/EmaillistApp.js
@@ -6,16 +6,44 @@ export default class EmaillistApp extends React.Component {
     constructor(){
         super(...arguments);
         this.state = {
-            keyword: ''
+            keyword: '',
+            emails: null
         }
+        console.log('EmaillistApp:constructor()');
+    }
+
+    componentWillMount() {
+        console.log('EmaillistApp:componentWillMount()');
+    }   
+
+    onNotifyKeywordChange(keyword){
+        this.setState({
+            keyword: keyword
+        })
     }
 
     render() {
+        console.log('EmaillistApp:render()');
+
         return (
             <div className='EmaillisApp'>
-                <SearchBar />
-                <Emaillist keyword={ this.state.keyword } emails={ this.props.emails }/>
+                <SearchBar keyword={ this.state.keyword } notifyKeywordChangeHandler={ this.onNotifyKeywordChange.bind(this) } />
+                <Emaillist keyword={ this.state.keyword } emails={ this.state.emails }/>
             </div>
         );
+    }
+
+    componentDidMount() {
+        console.log('EmaillistApp:componentDidMount()');
+        fetch('http://localhost:8888/data.json')
+        .then( response => response.json())
+        .then( json => this.setState({
+            emails: json
+        }))
+        .catch( err => console.error( err ));
+    }
+
+    componentWillUnmount() {
+        console.log('EmaillistApp:componentWillUnmount()');
     }
 }
